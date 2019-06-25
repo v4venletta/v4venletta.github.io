fc8143aaf55182777416f9fb3a8bf7e6e452b0e4
v4venletta <52084013+v4venletta@users.noreply.github.com>
2019-06-25 15:08:53 -0400

    changing "points"

--- data/am_base_player.js
+++ data/am_base_player.js
@@ -1,121 +1,121 @@
 [
 {
     "name": "am-p-01",
-    "points": 30,
+    "points": 0,
     "image": "attack-modifiers/base/player/am-p-01.png",
     "xws": "amp01"
   },
   {
     "name": "am-p-02",
-    "points": 31,
+    "points": 1,
     "image": "attack-modifiers/base/player/am-p-02.png",
     "xws": "amp02"
   },
   {
     "name": "am-p-03",
-    "points": 32,
+    "points": 2,
     "image": "attack-modifiers/base/player/am-p-03.png",
     "xws": "amp03"
   },
   {
     "name": "am-p-04",
-    "points": 33,
+    "points": 3,
     "image": "attack-modifiers/base/player/am-p-04.png",
     "xws": "amp04"
   },
   {
     "name": "am-p-05",
-    "points": 34,
+    "points": 4,
     "image": "attack-modifiers/base/player/am-p-05.png",
     "xws": "amp05"
   },
   {
     "name": "am-p-06",
-    "points": 35,
+    "points": 5,
     "image": "attack-modifiers/base/player/am-p-06.png",
     "xws": "amp06"
   },
   {
     "name": "am-p-07",
-    "points": 36,
+    "points": 6,
     "image": "attack-modifiers/base/player/am-p-07.png",
     "xws": "amp07"
   },
   {
     "name": "am-p-08",
-    "points": 37,
+    "points": 7,
     "image": "attack-modifiers/base/player/am-p-08.png",
     "xws": "amp08"
   },
   {
     "name": "am-p-09",
-    "points": 38,
+    "points": 8,
     "image": "attack-modifiers/base/player/am-p-09.png",
     "xws": "amp09"
   },
   {
     "name": "am-p-10",
-    "points": 39,
+    "points": 9,
     "image": "attack-modifiers/base/player/am-p-10.png",
     "xws": "amp10"
   },
   {
     "name": "am-p-11",
-    "points": 40,
+    "points": 10,
     "image": "attack-modifiers/base/player/am-p-11.png",
     "xws": "amp11"
   },
   {
     "name": "am-p-12",
-    "points": 41,
+    "points": 11,
     "image": "attack-modifiers/base/player/am-p-12.png",
     "xws": "amp12"
   },
   {
     "name": "am-p-13",
-    "points": 42,
+    "points": 12,
     "image": "attack-modifiers/base/player/am-p-13.png",
     "xws": "amp13"
   },
   {
     "name": "am-p-14",
-    "points": 43,
+    "points": 13,
     "image": "attack-modifiers/base/player/am-p-14.png",
     "xws": "amp14"
   },
   {
     "name": "am-p-15",
-    "points": 44,
+    "points": 14,
     "image": "attack-modifiers/base/player/am-p-15.png",
     "xws": "amp15"
   },
   {
     "name": "am-p-16",
-    "points": 45,
+    "points": 15,
     "image": "attack-modifiers/base/player/am-p-16.png",
     "xws": "amp16"
   },
   {
     "name": "am-p-17",
-    "points": 46,
+    "points": 16,
     "image": "attack-modifiers/base/player/am-p-17.png",
     "xws": "amp17"
   },
   {
     "name": "am-p-18",
-    "points": 47,
+    "points": 17,
     "image": "attack-modifiers/base/player/am-p-18.png",
     "xws": "amp18"
   },
   {
     "name": "am-p-19",
-    "points": 48,
+    "points": 18,
     "image": "attack-modifiers/base/player/am-p-19.png",
     "xws": "amp19"
   },
   {
     "name": "am-p-20",
-    "points": 49,
+    "points": 19,
     "image": "attack-modifiers/base/player/am-p-20.png",
     "xws": "amp20"
   }
