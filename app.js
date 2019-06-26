fe43a6f0583cf4c529d89454f19bec5a7945fe2d
v4venletta <52084013+v4venletta@users.noreply.github.com>
2019-06-26 17:43:46 -0400

    Adding all the node/babel data

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("Hello world");
+import {MDCDrawer} from "@material/drawer";
+const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+
+import {MDCTopAppBar} from "@material/top-app-bar";
+const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
+topAppBar.setScrollTarget(document.getElementById('main-content'));
+topAppBar.listen('MDCTopAppBar:nav', () => {
+  drawer.open = !drawer.open;
+});
---
+++ src/base.js
@@ -0,0 +1,2 @@
+import {MDCDrawer} from "@material/drawer";
+const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
