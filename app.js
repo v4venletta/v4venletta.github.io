71b1327ce65da2158c2f1f5d1b4a31973f72c29e
v4venletta <52084013+v4venletta@users.noreply.github.com>
2019-06-27 15:23:58 -0400

    adding drop down

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-console.log("Hello world");
 import {MDCDrawer} from "@material/drawer";
 import {MDCList} from "@material/list";
 import {MDCTopAppBar} from "@material/top-app-bar";
-
+import {MDCSelect} from '@material/select';
+import {MDCMenu} from '@material/menu';
 
 const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
-
+const menu = new MDCMenu(document.querySelector('.mdc-menu'));
 const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
+const select = new MDCSelect(document.querySelector('.mdc-select'));
+
+/*
+select.listen('MDCSelect:change', () => {
+  alert(`Selected option at index ${select.selectedIndex} with value "${select.value}"`);
+});
+*/
 topAppBar.setScrollTarget(document.getElementById('main-content'));
 topAppBar.listen('MDCTopAppBar:nav', () => {
   drawer.open = !drawer.open;
@@ -22,3 +29,4 @@ listEl.addEventListener('click', (event) => {
 mainContentEl.addEventListener('click', (event) => {
 drawer.open = false;
 });
+
