e8152dbaaabe9e654626ca57238dec93a02d3bcb
v4venletta <52084013+v4venletta@users.noreply.github.com>
2019-07-09 21:00:55 -0400

    adding modal, adding discard pile

--- app.js
+++ app.js
@@ -13,18 +13,31 @@ import {
 import {
   MDCMenu
 } from '@material/menu';
+import {MDCDialog} from '@material/dialog';
+
+const dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
 const select = new MDCSelect(document.querySelector('.mdc-select'));
 const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
 const listEl = document.querySelector('.mdc-drawer .mdc-list');
 const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
+var fullDiscard = document.getElementById("discardpile"); 
+
+dialog.listen('MDCDialog:opened', function() {
+  // Assuming contentElement references a common parent element with the rest of the page's content
+  contentElement.setAttribute('aria-hidden', 'true');
+});
+
+dialog.listen('MDCDialog:closing', function() {
+  contentElement.removeAttribute('aria-hidden');
+});
 
 topAppBar.setScrollTarget(document.getElementById('main-content'));
 topAppBar.listen('MDCTopAppBar:nav', () => {
   drawer.open = !drawer.open;
 });
 
-var activeCharacter;
-localStorage.removeItem("Character(test7)");
+var activeCharacter = new Character('base','');
+//localStorage.removeItem("Character(test7)");
 select.listen('MDCSelect:change', () => {
   if (activeCharacter === undefined) {
     document.getElementById("chosen-class").src = `/images/class-icons/${select.value}.png`;
@@ -36,6 +49,7 @@ select.listen('MDCSelect:change', () => {
     alert(`${select.value}` + ':' + activeCharacter.class);
   }
 });
+
 document.getElementById("hand").addEventListener('click', (event) => {
   var discardPile = document.getElementById("discard");
   var tempDeck = activeCharacter.deck;
@@ -46,21 +60,35 @@ document.getElementById("hand").addEventListener('click', (event) => {
   var stats = document.getElementById("stats");
 
   stats.innerHTML="Draw Pile: " + tempDeck.drawPile.length + ", Discard Pile: " + tempDeck.discardPile.length;
-  //console.log(drawnCard);
-  //console.log(Object.getOwnPropertyNames(Object.getPrototypeOf(activeCharacter)));
-  //console.log(tempDeck.draw());
   discardPile.src = '/images/' + drawnCard.image;
+  if (drawnCard.value === "x2" || drawnCard.value === "miss") {
+	var warning = document.getElementById("warning");
+  	warning.style = "display:inline;color:red;";
+  }
+  
+});
+
+document.getElementById("discard").addEventListener('click', (event) => {
+	var pileHTML = '';
+	for (var i = activeCharacter.deck.discardPile.length-1;i>=0; i--){
+		pileHTML = pileHTML + '<img src="' +'/images/' + activeCharacter.deck.discardPile[i].image + '" class="rounded_s" style="display:inline-block;width:100px;">'
+	}
+	fullDiscard.innerHTML = pileHTML ;
 });
 
 document.getElementById("shuffle").addEventListener('click', (event) => {
   var discardPile = document.getElementById("discard");
   var tempDeck = activeCharacter.deck;
   tempDeck.shuffleAll();
-  //var drawnCard = tempDeck.draw();
-  //  console.log(drawnCard);
-  //console.log(Object.getOwnPropertyNames(Object.getPrototypeOf(activeCharacter)));
-  //console.log(tempDeck.draw());
+
   discardPile.src = '';
   var stats = document.getElementById("stats");
   stats.innerHTML="Draw Pile: " + tempDeck.drawPile.length + ", Discard Pile: " + tempDeck.discardPile.length;
+  var warning = document.getElementById("warning");
+  warning.style = "display:none;";
+
+});
+
+document.getElementById("flip").addEventListener('click',(event)=>{
+dialog.open();
 });
