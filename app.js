6963c3f4194fa5199e812924a5ada55e00edf67d
v4venletta <52084013+v4venletta@users.noreply.github.com>
2019-07-23 08:01:18 -0400

    bundling and formatting

--- app.js
+++ app.js
@@ -1,49 +1,106 @@
 import {
-  MDCDrawer
+    MDCDrawer
 } from "@material/drawer";
 import {
-  MDCList
+    MDCList
 } from "@material/list";
 import {
-  MDCTopAppBar
+    MDCTopAppBar
 } from "@material/top-app-bar";
 import {
-  MDCSelect
+    MDCSelect
 } from '@material/select';
 import {
-  MDCMenu
+    MDCMenu
 } from '@material/menu';
 import {
-  MDCDialog
+    MDCDialog
 } from '@material/dialog';
 import {
-  Deck
+    Deck
 } from 'deck';
 import {
-  Character
+    Character
 } from 'character';
 import anime from 'animejs/lib/anime.es.js';
 const characterClasses = {
-    beasttyrant: {name: "beast tyrant", abbr: "bt"},  
-    berserker: {name: "berserker", abbr: "be"}, 
-    bladeswarm: {name: "bladeswarm", abbr: "bs"},
-    brute: {name: "brute", abbr: "br"},
-    cragheart: {name: "cragheart", abbr: "ch"},
-    diviner: {name: "diviner", abbr: "dr"},
-    doomstalker: {name: "doomstalker", abbr: "ds"},
-    elementalist: {name: "elementalist", abbr: "el"},
-    mindthief: {name: "mindthief", abbr: "mt"},
-    nightshroud: {name: "nightshroud", abbr: "ns"},
-    plagueherald: {name: "plagueherald", abbr: "ph"},
-    quartermaster: {name: "quartermaster", abbr: "qm"},
-    sawbones: {name: "sawbones", abbr: "sb"},
-    scoundrel: {name: "scoundrel", abbr: "sc"},
-    soothsinger: {name: "soothsinger", abbr: "ss"},
-    spellweaver: {name: "spellweaver", abbr: "sw"},
-    summoner: {name: "summoner", abbr: "su"},
-    sunkeeper: {name: "sunkeeper", abbr: "sk"},
-    tinkerer: {name: "tinkerer", abbr: "ti"}
-  };
+    beasttyrant: {
+        name: "beast tyrant",
+        abbr: "bt"
+    },
+    berserker: {
+        name: "berserker",
+        abbr: "be"
+    },
+    bladeswarm: {
+        name: "bladeswarm",
+        abbr: "bs"
+    },
+    brute: {
+        name: "brute",
+        abbr: "br"
+    },
+    cragheart: {
+        name: "cragheart",
+        abbr: "ch"
+    },
+    diviner: {
+        name: "diviner",
+        abbr: "dr"
+    },
+    doomstalker: {
+        name: "doomstalker",
+        abbr: "ds"
+    },
+    elementalist: {
+        name: "elementalist",
+        abbr: "el"
+    },
+    mindthief: {
+        name: "mindthief",
+        abbr: "mt"
+    },
+    nightshroud: {
+        name: "nightshroud",
+        abbr: "ns"
+    },
+    plagueherald: {
+        name: "plagueherald",
+        abbr: "ph"
+    },
+    quartermaster: {
+        name: "quartermaster",
+        abbr: "qm"
+    },
+    sawbones: {
+        name: "sawbones",
+        abbr: "sb"
+    },
+    scoundrel: {
+        name: "scoundrel",
+        abbr: "sc"
+    },
+    soothsinger: {
+        name: "soothsinger",
+        abbr: "ss"
+    },
+    spellweaver: {
+        name: "spellweaver",
+        abbr: "sw"
+    },
+    summoner: {
+        name: "summoner",
+        abbr: "su"
+    },
+    sunkeeper: {
+        name: "sunkeeper",
+        abbr: "sk"
+    },
+    tinkerer: {
+        name: "tinkerer",
+        abbr: "ti"
+    }
+};
 
 const dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
 const select = new MDCSelect(document.querySelector('.mdc-select'));
@@ -55,151 +112,154 @@ var rotate = 0;
 
 topAppBar.setScrollTarget(document.getElementById('main-content'));
 topAppBar.listen('MDCTopAppBar:nav', () => {
-  drawer.open = !drawer.open;
+    drawer.open = !drawer.open;
 });
 
 //Instantiating a new Character
 try {
 
-  var activeCharacter = new Character('base', '');
-  console.log(activeCharacter);
+    var activeCharacter = new Character('base', '');
+    console.log(activeCharacter);
 } catch (err) {
-  console.log(err);
-  console.error(err);
+    console.log(err);
+    console.error(err);
 }
 
 //localStorage.removeItem("Character(test7)");
 select.listen('MDCSelect:change', () => {
-  console.log("select listener");
-  if (activeCharacter === undefined) {
-    document.getElementById("chosen-class").src = `/images/class-icons/${select.value}.png`;
-  } else if (activeCharacter.class != `${select.value}`) {
-    document.getElementById("chosen-class").src = `/images/class-icons/${select.value}.png`;
-    activeCharacter.class = `${select.value}`;
-    activeCharacter.setClass(activeCharacter.class)
-      .then(function(){
-        //Load the sheet checkmarks here.
-        console.log(activeCharacter);
-      });
-  }
+    console.log("select listener");
+    if (activeCharacter === undefined) {
+        document.getElementById("chosen-class").src = `/images/class-icons/${select.value}.png`;
+    } else if (activeCharacter.class != `${select.value}`) {
+        document.getElementById("chosen-class").src = `/images/class-icons/${select.value}.png`;
+        activeCharacter.class = `${select.value}`;
+        activeCharacter.setClass(activeCharacter.class)
+            .then(function() {
+                //Load the sheet checkmarks here.
+                console.log(activeCharacter);
+            });
+    }
 });
 
 
 //Add event listener to hand for handling cards being drawn
 document.getElementById("hand").addEventListener('click', (event) => {
-  $("#cards").slideUp("fast");
-  var discardPile = document.getElementById("discard");
-  var tempDeck = activeCharacter.deck;
-  //This is now handled by Deck.draw()
-  // if (tempDeck.drawPile.length === 0) {
-  //   tempDeck.shuffleAll();
-  // }
-  if (tempDeck.discardPile.length > 0) {
-  document.getElementById("discardParent").style.backgroundImage = "url('/images/" + tempDeck.discardPile[tempDeck.discardPile.length-1].image +"')";};
-  var drawnCard = tempDeck.draw();
-  var stats = document.getElementById("stats");
+    $("#cards").slideUp("fast");
+    var discardPile = document.getElementById("discard");
+    var tempDeck = activeCharacter.deck;
+    //This is now handled by Deck.draw()
+    // if (tempDeck.drawPile.length === 0) {
+    //   tempDeck.shuffleAll();
+    // }
+    if (tempDeck.discardPile.length > 0) {
+        document.getElementById("discardParent").style.backgroundImage = "url('/images/" + tempDeck.discardPile[tempDeck.discardPile.length - 1].image + "')";
+    };
+    var drawnCard = tempDeck.draw();
+    var stats = document.getElementById("stats");
 
-  stats.innerHTML = "Draw Pile: " + tempDeck.drawPile.length + ", Discard Pile: " + tempDeck.discardPile.length;
-  discardPile.src = '/images/' + drawnCard.image;
-  discardPile.style.opacity = 1.0;
-  if (screen.width <=600) {
-    console.log(screen.width);
-     anime({
-    targets: '#discard',
-     translateY: {
-    value: [-250,0],
-    duration: 800
-  }
-  });
-  } else {
-    console.log(screen.width);
-    anime({
-    targets: '#discard',
-     translateX: {
-    value: [-250,0],
-    duration: 800}});
-  }
-  if (activeCharacter.deck.shuffleNeeded) {
-    var warning = document.getElementById("warning");
-    warning.style = "display:inline;color:red;";
-  }
+    stats.innerHTML = "Draw Pile: " + tempDeck.drawPile.length + ", Discard Pile: " + tempDeck.discardPile.length;
+    discardPile.src = '/images/' + drawnCard.image;
+    discardPile.style.opacity = 1.0;
+    if (screen.width <= 600) {
+        console.log(screen.width);
+        anime({
+            targets: '#discard',
+            translateY: {
+                value: [-250, 0],
+                duration: 800
+            }
+        });
+    } else {
+        console.log(screen.width);
+        anime({
+            targets: '#discard',
+            translateX: {
+                value: [-250, 0],
+                duration: 800
+            }
+        });
+    }
+    if (activeCharacter.deck.shuffleNeeded) {
+        var warning = document.getElementById("warning");
+        warning.style = "display:inline;color:red;";
+    }
 });
 
 
 document.getElementById("bless").addEventListener('click', (event) => {
-  let blessingCard = {
-    "name": "am-pm-01",
-    "points": 50,
-    "image": "attack-modifiers/base/player-mod/am-pm-01.png",
-    "value": "bless",
-    "xws": "ampm01"
-  };
-  let tempDeck = activeCharacter.deck;
-  tempDeck.addCard(blessingCard);
-  let blessDeck = document.getElementById("modDeck");
-  blessDeck.src = "/images/attack-modifiers/base/player-mod/am-pm-01.png";
-  blessDeck.style.opacity = 1.0;
-  rotate = rotate + 360;
-  anime({
-    targets: '#modDeck',
-     translateX: {
-    value: [-250,0],
-    duration: 800
-  },
-  opacity:{
-    value: 0.0,
-    delay: 800,
-    easing: 'linear'
-  },
-    rotate: rotate,
-    duration: 800
-  });
+    let blessingCard = {
+        "name": "am-pm-01",
+        "points": 50,
+        "image": "attack-modifiers/base/player-mod/am-pm-01.png",
+        "value": "bless",
+        "xws": "ampm01"
+    };
+    let tempDeck = activeCharacter.deck;
+    tempDeck.addCard(blessingCard);
+    let blessDeck = document.getElementById("modDeck");
+    blessDeck.src = "/images/attack-modifiers/base/player-mod/am-pm-01.png";
+    blessDeck.style.opacity = 1.0;
+    rotate = rotate + 360;
+    anime({
+        targets: '#modDeck',
+        translateX: {
+            value: [-250, 0],
+            duration: 800
+        },
+        opacity: {
+            value: 0.0,
+            delay: 800,
+            easing: 'linear'
+        },
+        rotate: rotate,
+        duration: 800
+    });
 
 });
 document.getElementById("curse").addEventListener('click', (event) => {
-  let curseCard = {
-    "name": "am-pm-21",
-    "points": 50,
-    "image": "attack-modifiers/base/player-mod/am-pm-21.png",
-    "value": "curse",
-    "xws": "ampm21"
-  };
-  let tempDeck = activeCharacter.deck;
-  tempDeck.addCard(curseCard);
-  let curseDeck = document.getElementById("modDeck");
-  curseDeck.src = "/images/attack-modifiers/base/player-mod/am-pm-21.png";
-  curseDeck.style.opacity = 1.0;
-  rotate = rotate + 360;
-  anime({
-    targets: '#modDeck',
-     translateX: {
-    value: [-250,0],
-    duration: 800
-  },
-  opacity:{
-    value: 0.0,
-    delay: 800,
-    easing: 'linear'
-  },
-    rotate: rotate,
-    duration: 800
-  });
+    let curseCard = {
+        "name": "am-pm-21",
+        "points": 50,
+        "image": "attack-modifiers/base/player-mod/am-pm-21.png",
+        "value": "curse",
+        "xws": "ampm21"
+    };
+    let tempDeck = activeCharacter.deck;
+    tempDeck.addCard(curseCard);
+    let curseDeck = document.getElementById("modDeck");
+    curseDeck.src = "/images/attack-modifiers/base/player-mod/am-pm-21.png";
+    curseDeck.style.opacity = 1.0;
+    rotate = rotate + 360;
+    anime({
+        targets: '#modDeck',
+        translateX: {
+            value: [-250, 0],
+            duration: 800
+        },
+        opacity: {
+            value: 0.0,
+            delay: 800,
+            easing: 'linear'
+        },
+        rotate: rotate,
+        duration: 800
+    });
 });
 document.getElementById("discard").addEventListener('click', (event) => {
 
-  var cardsDiv = document.getElementById("cards");
-  if (window.getComputedStyle(cardsDiv).display != "none") {
-    // Do something..
-    console.log("sliding up");
-    $('#cards').slideUp("fast");
-  } else {
-    $('#cards').slideDown("fast");
-    var pileHTML = '';
-    for (var i = activeCharacter.deck.discardPile.length - 1; i >= 0; i--) {
-      pileHTML = pileHTML + '<img src="' + '/images/' + activeCharacter.deck.discardPile[i].image + '" class="rounded_s" style="display:inline-block;width:100px;">'
+    var cardsDiv = document.getElementById("cards");
+    if (window.getComputedStyle(cardsDiv).display != "none") {
+        // Do something..
+        console.log("sliding up");
+        $('#cards').slideUp("fast");
+    } else {
+        $('#cards').slideDown("fast");
+        var pileHTML = '';
+        for (var i = activeCharacter.deck.discardPile.length - 1; i >= 0; i--) {
+            pileHTML = pileHTML + '<img src="' + '/images/' + activeCharacter.deck.discardPile[i].image + '" class="rounded_s" style="display:inline-block;width:100px;">'
+        }
+        fullDiscard.innerHTML = pileHTML;
     }
-    fullDiscard.innerHTML = pileHTML;
-  }
 
 
 });
@@ -207,48 +267,48 @@ document.getElementById("discard").addEventListener('click', (event) => {
 
 //Adding event listener to handle shuffling
 document.getElementById("shuffle").addEventListener('click', (event) => {
-  var discardPile = document.getElementById("discard");
-  var tempDeck = activeCharacter.deck;
-  tempDeck.shuffleAll();
-  discardPile.src = 'images/attack-modifiers/top-small.jpg';
-  discardPile.style.opacity = 0.0;
-   document.getElementById("discardParent").style.backgroundImage = "";
-  var stats = document.getElementById("stats");
-  stats.innerHTML = "Draw Pile: " + tempDeck.drawPile.length + ", Discard Pile: " + tempDeck.discardPile.length;
-  var warning = document.getElementById("warning");
-  warning.style = "display:none;";
+    var discardPile = document.getElementById("discard");
+    var tempDeck = activeCharacter.deck;
+    tempDeck.shuffleAll();
+    discardPile.src = 'images/attack-modifiers/top-small.jpg';
+    discardPile.style.opacity = 0.0;
+    document.getElementById("discardParent").style.backgroundImage = "";
+    var stats = document.getElementById("stats");
+    stats.innerHTML = "Draw Pile: " + tempDeck.drawPile.length + ", Discard Pile: " + tempDeck.discardPile.length;
+    var warning = document.getElementById("warning");
+    warning.style = "display:none;";
 
 });
 
 //Handle building the elements inside the modal window
 document.getElementById("flip").addEventListener('click', (event) => {
-  let tempDeck = activeCharacter.deck;
-  let drawnCards = tempDeck.drawCards(2);
-  let stats = document.getElementById("stats");
-  let card1 = document.getElementById("recent");
-  let card2 = document.getElementById("recent-1");
+    let tempDeck = activeCharacter.deck;
+    let drawnCards = tempDeck.drawCards(2);
+    let stats = document.getElementById("stats");
+    let card1 = document.getElementById("recent");
+    let card2 = document.getElementById("recent-1");
 
-  stats.innerHTML = "Draw Pile: " + tempDeck.drawPile.length + ", Discard Pile: " + tempDeck.discardPile.length;
-  card1.src = '/images/' + drawnCards[0].image;
-  card1.attributes["data-mdc-dialog-action"].value = card1.src;
-  card2.src = '/images/' + drawnCards[1].image;
-  card2.attributes["data-mdc-dialog-action"].value = card2.src;
+    stats.innerHTML = "Draw Pile: " + tempDeck.drawPile.length + ", Discard Pile: " + tempDeck.discardPile.length;
+    card1.src = '/images/' + drawnCards[0].image;
+    card1.attributes["data-mdc-dialog-action"].value = card1.src;
+    card2.src = '/images/' + drawnCards[1].image;
+    card2.attributes["data-mdc-dialog-action"].value = card2.src;
 
-  dialog.open();
+    dialog.open();
 });
 
 dialog.listen('MDCDialog:opened', (event) => {
-  console.log("Dialog Opened");
+    console.log("Dialog Opened");
 });
 
 dialog.listen('MDCDialog:closed', (event) => {
-  if (activeCharacter.deck.shuffleNeeded) {
-    document.getElementById("warning").style = "display:inline;color:red;";
-  }
-  if (event.detail.action == "close") {
-    document.getElementById("discard").src = '/images/' + activeCharacter.deck.discardPile[activeCharacter.deck.discardPile.length - 1].image;
-  } else {
-    document.getElementById("discard").src = event.detail.action;
-  }
+    if (activeCharacter.deck.shuffleNeeded) {
+        document.getElementById("warning").style = "display:inline;color:red;";
+    }
+    if (event.detail.action == "close") {
+        document.getElementById("discard").src = '/images/' + activeCharacter.deck.discardPile[activeCharacter.deck.discardPile.length - 1].image;
+    } else {
+        document.getElementById("discard").src = event.detail.action;
+    }
 
 });
