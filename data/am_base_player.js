3941a00934f49a127631f800987ab126b05a0448
v4venletta <52084013+v4venletta@users.noreply.github.com>
2019-07-23 10:19:52 -0400

    reusing images for same cards

--- data/am_base_player.js
+++ data/am_base_player.js
@@ -8,31 +8,31 @@
   {
     "name": "am-p-02",
     "points": 1,
-    "image": "attack-modifiers/base/player/am-p-02.png",
+    "image": "attack-modifiers/base/player/am-p-01.png",
     "xws": "amp02"
   },
   {
     "name": "am-p-03",
     "points": 2,
-    "image": "attack-modifiers/base/player/am-p-03.png",
+    "image": "attack-modifiers/base/player/am-p-01.png",
     "xws": "amp03"
   },
   {
     "name": "am-p-04",
     "points": 3,
-    "image": "attack-modifiers/base/player/am-p-04.png",
+    "image": "attack-modifiers/base/player/am-p-01.png",
     "xws": "amp04"
   },
   {
     "name": "am-p-05",
     "points": 4,
-    "image": "attack-modifiers/base/player/am-p-05.png",
+    "image": "attack-modifiers/base/player/am-p-01.png",
     "xws": "amp05"
   },
   {
     "name": "am-p-06",
     "points": 5,
-    "image": "attack-modifiers/base/player/am-p-06.png",
+    "image": "attack-modifiers/base/player/am-p-01.png",
     "xws": "amp06"
   },
   {
@@ -44,25 +44,25 @@
   {
     "name": "am-p-08",
     "points": 7,
-    "image": "attack-modifiers/base/player/am-p-08.png",
+    "image": "attack-modifiers/base/player/am-p-07.png",
     "xws": "amp08"
   },
   {
     "name": "am-p-09",
     "points": 8,
-    "image": "attack-modifiers/base/player/am-p-09.png",
+    "image": "attack-modifiers/base/player/am-p-07.png",
     "xws": "amp09"
   },
   {
     "name": "am-p-10",
     "points": 9,
-    "image": "attack-modifiers/base/player/am-p-10.png",
+    "image": "attack-modifiers/base/player/am-p-07.png",
     "xws": "amp10"
   },
   {
     "name": "am-p-11",
     "points": 10,
-    "image": "attack-modifiers/base/player/am-p-11.png",
+    "image": "attack-modifiers/base/player/am-p-07.png",
     "xws": "amp11"
   },
   {
@@ -74,25 +74,25 @@
   {
     "name": "am-p-13",
     "points": 12,
-    "image": "attack-modifiers/base/player/am-p-13.png",
+    "image": "attack-modifiers/base/player/am-p-12.png",
     "xws": "amp13"
   },
   {
     "name": "am-p-14",
     "points": 13,
-    "image": "attack-modifiers/base/player/am-p-14.png",
+    "image": "attack-modifiers/base/player/am-p-12.png",
     "xws": "amp14"
   },
   {
     "name": "am-p-15",
     "points": 14,
-    "image": "attack-modifiers/base/player/am-p-15.png",
+    "image": "attack-modifiers/base/player/am-p-12.png",
     "xws": "amp15"
   },
   {
     "name": "am-p-16",
     "points": 15,
-    "image": "attack-modifiers/base/player/am-p-16.png",
+    "image": "attack-modifiers/base/player/am-p-12.png",
     "xws": "amp16"
   },
   {
